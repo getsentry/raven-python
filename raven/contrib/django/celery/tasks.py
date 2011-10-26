70b07b02a15fbd5a8e9edd1f0e5ae98b8fe1935e
David Cramer <dcramer@gmail.com>
2011-10-25 18:26:58 -0700

    missing file header

--- raven/contrib/django/celery/tasks.py
+++ raven/contrib/django/celery/tasks.py
@@ -1,3 +1,11 @@
+"""
+raven.contrib.django.celery.tasks
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2010 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+
 # TODO: need to educate myself on how this works
 
 from raven.contrib.django.models import get_client
