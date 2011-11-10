cf62f83404b950151f7e0ca9bcfc09c3d0fd2fd9
David Cramer <dcramer@gmail.com>
2011-11-10 13:53:16 -0800

    Dont store line numbers with pre/post context in templates

--- raven/contrib/django/utils.py
+++ raven/contrib/django/utils.py
@@ -16,7 +16,7 @@ def get_data_from_template(source):
     for num, next in enumerate(linebreak_iter(template_source)):
         if start >= upto and end <= next:
             lineno = num
-        source_lines.append((num, template_source[upto:next]))
+        source_lines.append(template_source[upto:next])
         upto = next
 
     if not source_lines or lineno is None:
@@ -30,7 +30,7 @@ def get_data_from_template(source):
         'sentry.interfaces.Template': {
             'filename': origin.name,
             'pre_context': pre_context,
-            'context_line': context_line[1],
+            'context_line': context_line,
             'lineno': lineno,
             'post_context': post_context,
         },
