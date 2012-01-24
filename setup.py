7431bc4450c5be133bbc725f199f3a69ac31c28a
David Cramer <dcramer@gmail.com>
2012-01-23 17:51:51 -0800

    Version bump to 1.1.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ install_requires = [
 
 setup(
     name='raven',
-    version='1.1.0',
+    version='1.1.1',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/raven',
