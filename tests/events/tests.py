1cbc3fa8f9ca1779d29ace89241429dce2f82c0a
Pound Marshall <marshall@poundpay.com>
2012-01-25 08:04:40 -0800

    handle variable number of args missing from message

--- tests/events/tests.py
+++ tests/events/tests.py
@@ -6,18 +6,26 @@ from raven.events import Message
 
 
 class MessageTest(TestCase):
+
     def test_to_string(self):
+        unformatted_message = 'My message from %s about %s'
         client = Mock()
         message = Message(client)
-
+        message.logger = Mock()
         data = {
             'sentry.interfaces.Message': {
-                'message': 'My message from %s about %s',
+                'message': unformatted_message,
             }
         }
-        self.assertEqual(message.to_string(data),
-                         'My message from UNDEFINED about UNDEFINED')
+
+        self.assertEqual(message.to_string(data), unformatted_message)
+        self.assertEqual(message.logger.warn.call_count, 1)
+
+        args, kwargs = message.logger.warn.call_args
+        self.assertEqual(args, ('Unable to find params for message',))
+        self.assertEqual(kwargs,
+                         {'extra': {'msg': {'message': unformatted_message}}})
 
         data['sentry.interfaces.Message']['params'] = (1, 2)
         self.assertEqual(message.to_string(data),
-                         'My message from 1 about 2')
+                         unformatted_message % (1, 2))
