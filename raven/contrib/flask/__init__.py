7debdf82f9a4d57336200d67e01dce74c9c6b4db
David Cramer <dcramer@gmail.com>
2012-01-29 21:13:00 -0800

    Added logging and DSN support to Flask client

--- raven/contrib/flask/__init__.py
+++ raven/contrib/flask/__init__.py
@@ -10,34 +10,46 @@ from __future__ import absolute_import
 
 from flask import request
 from flask.signals import got_request_exception
+from raven.conf import setup_logging
 from raven.base import Client
 from raven.contrib.flask.utils import get_data_from_request
+from raven.handlers.logging import SentryHandler
 
 
 class Sentry(object):
-    def __init__(self, app=None, client=None, client_cls=Client):
+    """
+    Flask application for Sentry.
+
+    >>> # Look up configuration from ``os.environ['SENTRY_DSN']``
+    >>> sentry = Sentry(app)
+
+    >>> # Pass an arbitrary dsn
+    >>> sentry = Sentry(app, dsn='http://public:secret@example.com/1')
+
+    >>> # Pass an explicit client
+    >>> sentry = Sentry(app, client=client)
+
+    >>> # Automatically configure logging
+    >>> sentry = Sentry(app, logging=True)
+    """
+    def __init__(self, app=None, client=None, client_cls=Client, dsn=None,
+                 logging=False):
         self.app = app
-        self.client = client
         self.client_cls = client_cls
+        self.dsn = dsn
+        self.logging = logging
+        self._client = client
 
         if app:
             self.init_app(app)
 
-    def handle_exception(self, client):
-        def _handle_exception(sender, **kwargs):
-            client.capture('Exception', exc_info=kwargs.get('exc_info'),
-                data=get_data_from_request(request),
-                extra={
-                    'app': sender.name,
-                },
-            )
-        return _handle_exception
-
-    def init_app(self, app):
-        if not self.client:
-            if not app.config.get('SENTRY_SERVERS'):
-                raise TypeError('The SENTRY_SERVERS config variable is required.')
-            client = self.client_cls(
+    @property
+    def client(self):
+        app = self.app
+        if app is None:
+            return None
+        if self._client is None:
+            self._client = self.client_cls(
                 include_paths=set(app.config.get('SENTRY_INCLUDE_PATHS', [])) | set([app.import_name]),
                 exclude_paths=app.config.get('SENTRY_EXCLUDE_PATHS'),
                 servers=app.config.get('SENTRY_SERVERS'),
@@ -47,9 +59,21 @@ class Sentry(object):
                 secret_key=app.config.get('SENTRY_SECRET_KEY'),
                 project=app.config.get('SENTRY_PROJECT'),
                 site=app.config.get('SENTRY_SITE_NAME'),
+                dsn=app.config.get('SENTRY_DSN'),
             )
-            self.client = client
-        else:
-            client = self.client
+        return self._client
 
-        got_request_exception.connect(self.handle_exception(client), sender=app, weak=False)
+    def handle_exception(self, client):
+        def _handle_exception(*args, **kwargs):
+            client.capture('Exception', exc_info=kwargs.get('exc_info'),
+                data=get_data_from_request(request),
+                extra={
+                    'app': self.app,
+                },
+            )
+        return _handle_exception
+
+    def init_app(self, app):
+        if self.logging:
+            setup_logging(SentryHandler(self.client))
+        got_request_exception.connect(self.handle_exception(self.client), sender=app, weak=False)
