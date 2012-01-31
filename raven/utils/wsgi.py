9d5befa99644e28400e1659760975c593a3dc2da
David Cramer <dcramer@gmail.com>
2012-01-31 14:33:33 -0800

    Handle retarded keys in environment (fixes GH-51)

--- raven/utils/wsgi.py
+++ raven/utils/wsgi.py
@@ -14,6 +14,7 @@ def get_headers(environ):
     Returns only proper HTTP headers.
     """
     for key, value in environ.iteritems():
+        key = str(key)
         if key.startswith('HTTP_') and key not in \
            ('HTTP_CONTENT_TYPE', 'HTTP_CONTENT_LENGTH'):
             yield key[5:].replace('_', '-').title(), value
