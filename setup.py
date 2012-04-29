e511d9d7b6a6152bf584f72621de7c389c3f78dd
Marc Abramowitz <marc@marc-abramowitz.com>
2012-04-29 16:59:54 -0700

    Hack to prevent stupid "TypeError: 'NoneType' object is not callable" error in multiprocessing/util.py _exit_function when running `python setup.py test` (see http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html)

--- setup.py
+++ setup.py
@@ -9,6 +9,15 @@ Django, and Flask. Raven also includes drop-in support for any WSGI-compatible
 web application.
 """
 
+# Hack to prevent stupid "TypeError: 'NoneType' object is not callable" error
+# in multiprocessing/util.py _exit_function when running `python
+# setup.py test` (see
+# http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html)
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
 from setuptools import setup, find_packages
 
 tests_require = [
