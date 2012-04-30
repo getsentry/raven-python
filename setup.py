7590e16e793671b751a54fb1d7eb5cc608f0d916
Marc Abramowitz <marc@marc-abramowitz.com>
2012-04-30 12:07:29 -0700

    setup.py tweaks to improve the text on the PyPI page.

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 Raven
 ======
 
-Raven is a Python client for `Sentry <http://aboutsentry.com/>`_. It provides
+Raven is a Python client for `Sentry <http://getsentry.com/>`_. It provides
 full out-of-the-box support for many of the popular frameworks, including
-Django, and Flask. Raven also includes drop-in support for any WSGI-compatible
-web application.
+`Django <djangoproject.com>`_, `Flask <http://flask.pocoo.org/>`_, and `Pylons
+<http://www.pylonsproject.org/>`_. Raven also includes drop-in support for any
+`WSGI <http://wsgi.readthedocs.org/>`_-compatible web application.
 """
 
 from setuptools import setup, find_packages
