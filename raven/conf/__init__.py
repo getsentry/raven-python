getsentry/raven-python

commit b8ec5cef83a073796dc3fcde52dbb723951c96d9

Add a few more tests and fix handling of https and port 443

raven/conf/__init__.py
@@ -32,7 +32,7 @@ def load(dsn, scope=None):
     if url.scheme not in ('http', 'https', 'udp'):
         raise ValueError('Unsupported Sentry DSN scheme: %r' % url.scheme)
     netloc = url.hostname
-    if url.port and url.port != 80:
+    if (url.scheme == 'http' and url.port and url.port != 80) or (url.scheme == 'https' and url.port and url.port != 443):
         netloc += ':%s' % url.port
     path_bits = url.path.rsplit('/', 1)
     if len(path_bits) > 1:

tests/config/tests.py
@@ -48,6 +48,39 @@ class LoadTest(TestCase):
             'SENTRY_SECRET_KEY': 'bar',
         })
 
+    def test_http(self):
+        dsn = 'http://foo:bar@sentry.local/app/1'
+        res = {}
+        load(dsn, res)
+        self.assertEquals(res, {
+            'SENTRY_PROJECT': '1',
+            'SENTRY_SERVERS': ['http://sentry.local/app/api/store/'],
+            'SENTRY_PUBLIC_KEY': 'foo',
+            'SENTRY_SECRET_KEY': 'bar',
+        })
+
+    def test_http_with_port(self):
+        dsn = 'http://foo:bar@sentry.local:9000/app/1'
+        res = {}
+        load(dsn, res)
+        self.assertEquals(res, {
+            'SENTRY_PROJECT': '1',
+            'SENTRY_SERVERS': ['http://sentry.local:9000/app/api/store/'],
+            'SENTRY_PUBLIC_KEY': 'foo',
+            'SENTRY_SECRET_KEY': 'bar',
+        })
+
+    def test_https_port_443(self):
+        dsn = 'https://foo:bar@sentry.local:443/app/1'
+        res = {}
+        load(dsn, res)
+        self.assertEquals(res, {
+            'SENTRY_PROJECT': '1',
+            'SENTRY_SERVERS': ['https://sentry.local/app/api/store/'],
+            'SENTRY_PUBLIC_KEY': 'foo',
+            'SENTRY_SECRET_KEY': 'bar',
+        })
+
     def test_missing_netloc(self):
         dsn = 'https://foo:bar@/1'
         self.assertRaises(ValueError, load, dsn)

commit 4d8ae8b93ed53d3a2d2c1698afa630e310ed0383

Fix bad port handling in UDP DSNs
This was broken in b8ec5cef83a073796dc3fcde52dbb723951c96d9.

raven/conf/__init__.py
@@ -32,7 +32,7 @@ def load(dsn, scope=None):
     if url.scheme not in ('http', 'https', 'udp'):
         raise ValueError('Unsupported Sentry DSN scheme: %r' % url.scheme)
     netloc = url.hostname
-    if (url.scheme == 'http' and url.port and url.port != 80) or (url.scheme == 'https' and url.port and url.port != 443):
+    if url.port and (url.scheme, url.port) not in (('http', 80), ('https', 443)):
         netloc += ':%s' % url.port
     path_bits = url.path.rsplit('/', 1)
     if len(path_bits) > 1:

tests/config/tests.py
@@ -96,9 +96,12 @@ class LoadTest(TestCase):
         dsn = 'udp://foo:bar@sentry.local:9001/1'
         res = {}
         load(dsn, res)
-        self.assertEqual('1', res['SENTRY_PROJECT'])
-        self.assertEqual('foo', res['SENTRY_PUBLIC_KEY'])
-        self.assertEqual('bar', res['SENTRY_SECRET_KEY'])
+        self.assertEquals(res, {
+            'SENTRY_PROJECT': '1',
+            'SENTRY_SERVERS': ['udp://sentry.local:9001/api/store/'],
+            'SENTRY_PUBLIC_KEY': 'foo',
+            'SENTRY_SECRET_KEY': 'bar',
+        })
 
     def test_missing_netloc(self):
         dsn = 'https://foo:bar@/1'