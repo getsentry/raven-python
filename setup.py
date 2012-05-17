dd0d43faf143f59f452372863b30d26cd9d814d1
David Cramer <dcramer@gmail.com>
2012-05-16 18:02:17 -0700

    1.8.0

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ install_requires = [
 
 setup(
     name='raven',
-    version='1.7.7',
+    version='1.8.0',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/raven',
