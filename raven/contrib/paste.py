231f5f8de8f9ee5ce35a58a172bae0defb6172bd
David Cramer <dcramer@gmail.com>
2012-05-23 14:12:19 -0700

    pep8

--- raven/contrib/paste.py
+++ raven/contrib/paste.py
@@ -1,7 +1,7 @@
 from raven.middleware import Sentry
 from raven.base import Client
 
+
 def sentry_filter_factory(app, global_conf, **kwargs):
     client = Client(**kwargs)
     return Sentry(app, client)
-
