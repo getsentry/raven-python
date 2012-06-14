5622fd469cf8f1010bf8da9853fef6f18f731918
Patrick Gerken <do3cc@patrick-gerken.de>
2012-06-14 12:15:18 +0200

    Formatting and pathetic excuses

--- raven/contrib/zope/__init__.py
+++ raven/contrib/zope/__init__.py
@@ -1,13 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from inspect import getouterframes, currentframe, getinnerframes
 from raven.handlers.logging import SentryHandler
 from ZConfig.components.logger.factory import Factory
-from inspect import getouterframes, currentframe, getinnerframes
-from raven.utils.stacks import iter_stack_frames
-
 import logging
 
+from raven.utils.stacks import iter_stack_frames
+
+
 class ZopeSentryHandlerFactory(Factory):
+
     def getLevel(self):
-        return logging.WARNING
+        return self.section.level
 
     def create(self):
         return ZopeSentryHandler(**self.section.__dict__)
@@ -16,7 +21,15 @@ class ZopeSentryHandlerFactory(Factory):
         Factory.__init__(self)
         self.section = section
 
+
 class ZopeSentryHandler(SentryHandler):
+    """
+    Zope unfortunately eats the stack trace information.
+    To get the stack trace information and other useful information
+    from the request object, this class looks into the different stack
+    frames when the emit method is invoked.
+    """
+
     def __init__(self, *args, **kw):
         super(ZopeSentryHandler, self).__init__(*args, **kw)
         level = kw.get('level', logging.ERROR)
@@ -34,7 +47,8 @@ class ZopeSentryHandler(SentryHandler):
                     break
             if exc_info:
                 record.exc_info = exc_info
-                record.stack = iter_stack_frames(getinnerframes(exc_info[2]))
+                record.stack = \
+                    iter_stack_frames(getinnerframes(exc_info[2]))
             if request:
                 try:
                     body_pos = request.stdin.tell()
@@ -44,20 +58,16 @@ class ZopeSentryHandler(SentryHandler):
                     data = dict(headers=request.environ,
                                 url=request.getURL(),
                                 method=request.method,
-                                host=request.environ.get('REMOTE_ADDR', ''),
-                                data=body)
+                                host=request.environ.get('REMOTE_ADDR',
+                                ''), data=body)
                     if not hasattr(record, 'data'):
                         record.data = {}
                     record.data['sentry.interfaces.Http'] = data
                     user = request.AUTHENTICATED_USER
                     user_dict = dict(id=user.getId(),
-                                     is_authenticated=user.has_role('Authenticated'),
-                                     email='todo')
+                            is_authenticated=user.has_role('Authenticated'
+                            ), email='todo')
                     record.data['sentry.interfaces.User'] = user_dict
                 except (AttributeError, KeyError):
                     pass
         return super(ZopeSentryHandler, self).emit(record)
-
-
-            
-
