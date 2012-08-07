9ec2594f45e7ec9e6339b94875c3ff0e0a254cdd
Jaap Roes <jaap@eight.nl>
2012-08-07 09:49:56 +0200

    Avoid duplicating schemes

--- raven/utils/urlparse.py
+++ raven/utils/urlparse.py
@@ -4,7 +4,9 @@ import urlparse as _urlparse
 
 def register_scheme(scheme):
     for method in filter(lambda s: s.startswith('uses_'), dir(_urlparse)):
-        getattr(_urlparse, method).append(scheme)
+       	uses = getattr(_urlparse, method)
+        if scheme not in uses:
+            uses.append(scheme)
 
 
 urlparse = _urlparse.urlparse
