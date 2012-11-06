f5ca8503b48d1cf41a1e483e105c3fcb5e6cd018
David Cramer <dcramer@gmail.com>
2012-11-05 17:41:41 -0800

    Remove django model import as it did not solve gunicorn behavior (per 379adb5799f43fd71e5ed1176a514aec3dc75627)

--- raven/contrib/django/__init__.py
+++ raven/contrib/django/__init__.py
@@ -5,6 +5,6 @@ raven.contrib.django
 :copyright: (c) 2010-2012 by the Sentry Team, see AUTHORS for more details.
 :license: BSD, see LICENSE for more details.
 """
+from __future__ import absolute_import
 
 from raven.contrib.django.client import *
-from raven.contrib.django.models import *
