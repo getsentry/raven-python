910d352b5d1607fd9283fcdd53ee58e4366d7a6c
David Cramer <dcramer@gmail.com>
2012-12-18 00:07:52 -0800

    2.0.12.2 (missing requirements in dist)

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ tests_require = open(os.path.join(
 
 setup(
     name='raven',
-    version='2.0.12.1',
+    version='2.0.12.2',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/getsentry/raven-python',
