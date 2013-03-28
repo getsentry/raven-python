a632a652361bb35b0049da38253df239aef11b43
David Cramer <dcramer@gmail.com>
2013-03-28 15:12:20 -0700

    3.2.1

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ tests_require = [
 
 setup(
     name='raven',
-    version='3.2.0',
+    version='3.2.1',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/getsentry/raven-python',
