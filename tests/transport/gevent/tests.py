9ac0f2163ed95bb8d68adf4b7b3218fcfba67409
Luper Rouch <luper.rouch@gmail.com>
2013-06-05 12:37:47 +0200

    fixed the Greenlet.link() call in GeventedHTTPTransport; added a test to illustrate the issue (only fails on gevent 1.0)

---
+++ tests/transport/gevent/tests.py
@@ -0,0 +1,31 @@
+import mock
+import time
+import socket
+import gevent.monkey
+
+from raven.utils.testutils import TestCase
+from raven.base import Client
+
+
+class GeventTransportTest(TestCase):
+
+    def setUp(self):
+        gevent.monkey.patch_socket()
+        gevent.monkey.patch_time()
+        self.client = Client(
+            dsn="gevent+http://some_username:some_password@localhost:8143/1",
+        )
+
+    def tearDown(self):
+        # Undo gevent monkey patching
+        reload(socket)
+        reload(time)
+
+    @mock.patch('raven.transport.base.GeventedHTTPTransport._done')
+    @mock.patch('raven.transport.base.HTTPTransport.send')
+    def test_does_send(self, send, done):
+        self.client.captureMessage(message='foo')
+        time.sleep(0)
+        self.assertEqual(send.call_count, 1)
+        time.sleep(0)
+        self.assertEquals(done.call_count, 1)
