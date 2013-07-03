fddc5e2d29b205e8945cba403ae20469bbe5319e
Xavier Ordoquy <xordoquy@linovia.com>
2013-07-03 14:08:14 +0200

    Made the set / frozenset test more reliable.

--- tests/utils/json/tests.py
+++ tests/utils/json/tests.py
@@ -10,16 +10,16 @@ from raven.utils import json
 class JSONTest(TestCase):
     def test_uuid(self):
         res = uuid.uuid4()
-        self.assertEquals(json.dumps(res), '"%s"' % res.hex)
+        json.dumps(res) == '"%s"' % res.hex
 
     def test_datetime(self):
         res = datetime.datetime(day=1, month=1, year=2011, hour=1, minute=1, second=1)
-        self.assertEquals(json.dumps(res), '"2011-01-01T01:01:01Z"')
+        assert json.dumps(res) == '"2011-01-01T01:01:01Z"'
 
     def test_set(self):
         res = set(['foo', 'bar'])
-        self.assertEquals(json.dumps(res), '["foo", "bar"]')
+        assert json.dumps(res) in ('["foo", "bar"]', '["bar", "foo"]')
 
     def test_frozenset(self):
         res = frozenset(['foo', 'bar'])
-        self.assertEquals(json.dumps(res), '["foo", "bar"]')
+        assert json.dumps(res) in ('["foo", "bar"]', '["bar", "foo"]')
