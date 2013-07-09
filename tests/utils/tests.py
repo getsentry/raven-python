fe3f39b738b40c37e30b20fc16eade3cfcbc9cbf
Xavier Ordoquy <xordoquy@linovia.com>
2013-07-10 00:02:31 +0200

    Fully moved tests.utils.tests from unittest2 to py.test

--- tests/utils/tests.py
+++ tests/utils/tests.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from raven.utils.testutils import TestCase
-
 import raven
 from raven.utils import get_versions
 
 
-class GetVersionsTest(TestCase):
+class TestGetVersions(object):
     def test_exact_match(self):
         versions = get_versions(['raven'])
-        self.assertEquals(versions.get('raven'), raven.VERSION)
+        assert versions.get('raven') == raven.VERSION
 
     def test_parent_match(self):
         versions = get_versions(['raven.contrib.django'])
-        self.assertEquals(versions.get('raven'), raven.VERSION)
+        assert versions.get('raven') == raven.VERSION
