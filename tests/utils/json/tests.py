e10aafe23a6ea6c5eab0ea52968f317e0ca8d55c
Xavier Ordoquy <xordoquy@linovia.com>
2013-07-10 00:04:27 +0200

    Fully moved tests.utils.json.tests from unittest2 to py.test

--- tests/utils/json/tests.py
+++ tests/utils/json/tests.py
@@ -2,15 +2,14 @@
 
 import datetime
 import uuid
-from raven.utils.testutils import TestCase
 
 from raven.utils import json
 
 
-class JSONTest(TestCase):
+class TestJSON(object):
     def test_uuid(self):
         res = uuid.uuid4()
-        json.dumps(res) == '"%s"' % res.hex
+        assert json.dumps(res) == '"%s"' % res.hex
 
     def test_datetime(self):
         res = datetime.datetime(day=1, month=1, year=2011, hour=1, minute=1, second=1)
