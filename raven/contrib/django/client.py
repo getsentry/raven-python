19109f8f885f1075f98745817031e4ba77d9e555
Berkant AYDIN <berkant.aydin@markafoni.com>
2013-08-02 14:39:07 +0300

    post_sensitive_data acceptable dict and raven will really hide sensitive dict datas

--- raven/contrib/django/client.py
+++ raven/contrib/django/client.py
@@ -81,10 +81,19 @@ class DjangoClient(Client):
                     # assume we had a partial read:
                     data = '<unavailable>'
 
-			# hide sensitive data
+            # hide sensitive data
             if hasattr(request, 'sensitive_post_parameters'):
                 if request.sensitive_post_parameters == '__ALL__':
-                    data ='<hidden>'
+                    data = '<hidden>'
+                elif data != '<unavailable>':
+                    tmp = data.split('&')
+                    tmp = dict(keys.split('=') for keys in tmp)
+                    for param in request.sensitive_post_parameters:
+                        if param in tmp.keys():
+                            tmp[param] = '<hidden>'
+                    data = '&'.join(['%s=%s' % (key, value)
+                                     for (key, value) in tmp.items()])
+
         else:
             data = None
 
