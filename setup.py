ab53d276262a0fa2932588b34de4e68ffe22d930
David Cramer <dcramer@gmail.com>
2013-12-15 14:17:28 -0800

    3.6.1

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ class PyTest(TestCommand):
 
 setup(
     name='raven',
-    version='3.6.0',
+    version='3.6.1',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/getsentry/raven-python',
