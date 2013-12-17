892e503e2a092b290adc7368801affc17a81bff3
David Cramer <dcramer@gmail.com>
2013-12-17 02:01:04 -0800

    Minor cleanup to common processor functionality

--- raven/processors.py
+++ raven/processors.py
@@ -24,35 +24,41 @@ class Processor(object):
         resp = self.get_data(data, **kwargs)
         if resp:
             data = resp
+
+        if 'sentry.interfaces.Stacktrace' in data:
+            self.filter_stacktrace(data['sentry.interfaces.Stacktrace'])
+
+        if 'sentry.interfaces.Exception' in data:
+            if 'stacktrace' in data['sentry.interfaces.Exception']:
+                self.filter_stacktrace(data['sentry.interfaces.Exception']['stacktrace'])
+
+        if 'sentry.interfaces.Http' in data:
+            self.filter_http(data['sentry.interfaces.Http'])
+
         return data
+
+    def filter_stacktrace(self, data):
+        pass
+
+    def filter_http(self, data):
+        pass
 
 
 class RemovePostDataProcessor(Processor):
     """
     Removes HTTP post data.
     """
-    def process(self, data, **kwargs):
-        if 'sentry.interfaces.Http' in data:
-            data['sentry.interfaces.Http'].pop('data', None)
-
-        return data
+    def filter_http(self, data, **kwargs):
+        data.pop('data', None)
 
 
 class RemoveStackLocalsProcessor(Processor):
     """
     Removes local context variables from stacktraces.
     """
-    def process(self, data, **kwargs):
-        if 'sentry.interfaces.Stacktrace' in data:
-            for frame in data['sentry.interfaces.Stacktrace'].get('frames', []):
-                frame.pop('vars', None)
-
-        if 'sentry.interfaces.Exception' in data:
-            if 'stacktrace' in data['sentry.interfaces.Exception']:
-                for frame in data['sentry.interfaces.Exception']['stacktrace'].get('frames', []):
-                    frame.pop('vars', None)
-
-        return data
+    def filter_stacktrace(self, data, **kwargs):
+        for frame in data.get('frames', []):
+            frame.pop('vars', None)
 
 
 class SanitizePasswordsProcessor(Processor):
@@ -107,16 +113,3 @@ class SanitizePasswordsProcessor(Processor):
                 data[n] = '&'.join('='.join(k) for k in querybits)
             else:
                 data[n] = varmap(self.sanitize, data[n])
-
-    def process(self, data, **kwargs):
-        if 'sentry.interfaces.Stacktrace' in data:
-            self.filter_stacktrace(data['sentry.interfaces.Stacktrace'])
-
-        if 'sentry.interfaces.Exception' in data:
-            if 'stacktrace' in data['sentry.interfaces.Exception']:
-                self.filter_stacktrace(data['sentry.interfaces.Exception']['stacktrace'])
-
-        if 'sentry.interfaces.Http' in data:
-            self.filter_http(data['sentry.interfaces.Http'])
-
-        return data
