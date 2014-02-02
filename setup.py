a6fdb63ad390db6399423c01dcccb7e02252cb4f
Matt Robenolt <matt@ydekproductions.com>
2014-02-01 21:01:27 -0800

    4.0.4

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ class PyTest(TestCommand):
 
 setup(
     name='raven',
-    version='4.0.3',
+    version='4.0.4',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/getsentry/raven-python',
