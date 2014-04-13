b9e22384a40a3f407135e5a62810d15eb006162a
Matt Robenolt <matt@ydekproductions.com>
2014-04-13 18:14:07 -0400

    Fix date in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import datetime
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Raven'
-copyright = u'2010, David Cramer'
+copyright = u'%s, David Cramer' % datetime.datetime.today().year
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -186,8 +186,8 @@ htmlhelp_basename = 'Ravendoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'Raven.tex', u'Raven Documentation',
-   u'David Cramer', 'manual'),
+    ('index', 'Raven.tex', u'Raven Documentation',
+     u'David Cramer', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
