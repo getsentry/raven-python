aa9889b7afad4d2c0363ac4fe08271b7a2a6cb18
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-06-22 13:09:49 +0300

    Make style-checking tools happy.

--- raven/transport/aiohttp.py
+++ raven/transport/aiohttp.py
@@ -5,6 +5,10 @@ raven.transport.asyncio
 :copyright: (c) 2010-2014 by the Sentry Team, see AUTHORS for more details.
 :license: BSD, see LICENSE for more details.
 """
+# Skip flake8, python2 version doesn't recognize `yield from` statement
+# flake8: noqa
+from __future__ import absolute_import
+
 from raven.transport.base import AsyncTransport
 from raven.transport.http import HTTPTransport
 
