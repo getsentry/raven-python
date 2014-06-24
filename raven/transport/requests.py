ed9601b2899aef7fcadfe7306dc1320ce72f232c
Alexandru Chirila <alex@alexkiro.com>
2014-06-24 10:24:10 +0300

    Add support for the verify_ssl, ca_certs and timeout parameters for the request transport.

--- raven/transport/requests.py
+++ raven/transport/requests.py
@@ -7,6 +7,7 @@ raven.transport.requests
 """
 from __future__ import absolute_import
 
+from raven.conf import defaults
 from raven.transport.http import HTTPTransport
 
 try:
@@ -20,14 +21,23 @@ class RequestsHTTPTransport(HTTPTransport):
 
     scheme = ['requests+http', 'requests+https']
 
-    def __init__(self, parsed_url):
+    def __init__(self, parsed_url, timeout=defaults.TIMEOUT, verify_ssl=True,
+                 ca_certs=defaults.CA_BUNDLE):
         if not has_requests:
             raise ImportError('RequestsHTTPTransport requires requests.')
 
-        super(RequestsHTTPTransport, self).__init__(parsed_url)
+        super(RequestsHTTPTransport, self).__init__(parsed_url,
+                                                    timeout=timeout,
+                                                    verify_ssl=verify_ssl,
+                                                    ca_certs=ca_certs)
 
         # remove the requests+ from the protocol, as it is not a real protocol
         self._url = self._url.split('+', 1)[-1]
 
     def send(self, data, headers):
-        requests.post(self._url, data=data, headers=headers)
+        if self.verify_ssl:
+            # If SSL verification is enabled use the provided CA bundle to
+            # perform the verification.
+            self.verify_ssl = self.ca_certs
+        requests.post(self._url, data=data, headers=headers,
+                      verify=self.verify_ssl, timeout=self.timeout)
