cf487365047b9504721bbc2f230a3bc2f48bc4d9
Stefan Foulis <stefan@foulis.ch>
2014-10-29 14:19:38 +0100

    prevent label clashes in Django 1.7
    if there are multiple apps in the same project that end with ``.django`` there will be a label clash.

---
+++ raven/contrib/django/apps.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+
+class RavenConfig(AppConfig):
+    name = 'raven.contrib.django'
+    label = 'raven.contrib.django'
+    verbose_name = 'Raven'
