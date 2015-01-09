31746da0cbefccc5d6ee4211888c97bbfd9462bc
David Cramer <dcramer@gmail.com>
2015-01-09 15:12:23 -0600

    Bump version to 5.2.0 and note protocol change

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ class PyTest(TestCommand):
 
 setup(
     name='raven',
-    version='5.1.1',
+    version='5.2.0',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/getsentry/raven-python',
