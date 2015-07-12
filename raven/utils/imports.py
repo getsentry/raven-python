b69e6c52ffb4ba3fa5a15de29e60a6963c560a41
David Cramer <dcramer@gmail.com>
2015-07-12 11:55:20 -0600

    Dont specify level for import (refs GH-625)

--- raven/utils/imports.py
+++ raven/utils/imports.py
@@ -6,5 +6,5 @@ def import_string(key):
         return __import__(key)
 
     module_name, class_name = key.rsplit('.', 1)
-    module = __import__(module_name, {}, {}, [class_name], -1)
+    module = __import__(module_name, {}, {}, [class_name])
     return getattr(module, class_name)
