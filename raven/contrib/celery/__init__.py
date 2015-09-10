5b4e86cb0130167db4999b6441ddcb62905ad73f
David Cramer <dcramer@gmail.com>
2015-09-10 11:15:10 -0500

    Revert "redirect => redirected"
    This reverts commit d65cc551ca25327c365d4dbd02aaccba7c0077d6.

--- raven/contrib/celery/__init__.py
+++ raven/contrib/celery/__init__.py
@@ -14,10 +14,10 @@ from raven.handlers.logging import SentryHandler
 
 class CeleryFilter(logging.Filter):
     def filter(self, record):
-        # celery.redirected is used for stdout -- kill it
+        # celery.redirect is used for stdout -- kill it
         # TODO(dcramer): this should be more configurable, but our default
         # injections with Celery aren't super friendly towards configurability
-        if record.name == 'celery.redirected':
+        if record.name == 'celery.redirect':
             return False
 
         # Context is fixed in Celery 3.x so use internal flag instead
