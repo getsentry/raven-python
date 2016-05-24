08832aa36ec5aeada388fe7bdf28b9bd3f6490dc
David Cramer <dcramer@gmail.com>
2016-05-24 12:59:40 -0700

    Add fingerprint to soft timeouts in Celery

--- raven/contrib/celery/__init__.py
+++ raven/contrib/celery/__init__.py
@@ -9,6 +9,7 @@ from __future__ import absolute_import
 
 import logging
 
+from celery.exceptions import SoftTimeLimitExceeded
 from celery.signals import after_setup_logger, task_failure
 from raven.handlers.logging import SentryHandler
 
@@ -24,15 +25,21 @@ class CeleryFilter(logging.Filter):
 
 
 def register_signal(client):
-    def process_failure_signal(sender, task_id, args, kwargs, **kw):
+    def process_failure_signal(sender, task_id, args, kwargs, einfo, **kw):
         # This signal is fired inside the stack so let raven do its magic
+        if isinstance(einfo.exception, SoftTimeLimitExceeded):
+            fingerprint = ['celery', 'SoftTimeLimitExceeded', sender]
+        else:
+            fingerprint = None
         client.captureException(
             extra={
                 'task_id': task_id,
                 'task': sender,
                 'args': args,
                 'kwargs': kwargs,
-            })
+            },
+            fingerprint=fingerprint,
+        )
 
     task_failure.connect(process_failure_signal, weak=False)
 
