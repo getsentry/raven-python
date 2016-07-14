02aa7bee9addd1aaf2016af181c27a6ef9f37d01
Armin Ronacher <armin.ronacher@active-4.com>
2016-07-14 15:10:56 +0500

    5.23.0

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ class PyTest(TestCommand):
 
 setup(
     name='raven',
-    version='5.22.0',
+    version='5.23.0',
     author='Sentry',
     author_email='hello@getsentry.com',
     url='https://github.com/getsentry/raven-python',
