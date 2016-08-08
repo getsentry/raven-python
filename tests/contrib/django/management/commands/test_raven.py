e8aa3a2e60e9d2ac9b065e83a064055eac234867
David Cramer <dcramer@gmail.com>
2016-08-08 12:37:19 -0700

    Correct test for Django 1.8+

--- tests/contrib/django/management/commands/test_raven.py
+++ tests/contrib/django/management/commands/test_raven.py
@@ -14,7 +14,7 @@ DJANGO_18 = django.VERSION >= (1, 8, 0)
 class RavenCommandTest(TestCase):
     @patch('raven.contrib.django.management.commands.raven.send_test_message')
     def test_basic(self, mock_send_test_message):
-        call_command('raven', command='test')
+        call_command('raven', 'test')
 
         mock_send_test_message.assert_called_once_with(
             client, {
