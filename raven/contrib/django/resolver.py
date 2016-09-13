0142b7b3bd79fbd129121e4ab3d38cd8459bb048
David Cramer <dcramer@gmail.com>
2016-09-13 12:04:10 -0700

    Handle included patterns

--- raven/contrib/django/resolver.py
+++ raven/contrib/django/resolver.py
@@ -3,9 +3,9 @@ from __future__ import absolute_import
 import re
 
 try:
-    from django.urls import get_resolver, Resolver404
+    from django.urls import get_resolver
 except ImportError:
-    from django.core.urlresolvers import get_resolver, Resolver404
+    from django.core.urlresolvers import get_resolver
 
 
 class RouteResolver(object):
@@ -32,44 +32,58 @@ class RouteResolver(object):
         # TODO(dcramer): it'd be nice to change these into [%s] but it currently
         # conflicts with the other rules because we're doing regexp matches
         # rather than parsing tokens
-        pattern = self._optional_group_matcher.sub(lambda m: '%s' % m.group(1), pattern)
+        result = self._optional_group_matcher.sub(lambda m: '%s' % m.group(1), pattern)
 
         # handle named groups first
-        pattern = self._named_group_matcher.sub(lambda m: '{%s}' % m.group(1), pattern)
+        result = self._named_group_matcher.sub(lambda m: '{%s}' % m.group(1), result)
 
         # handle non-named groups
-        pattern = self._non_named_group_matcher.sub("{var}", pattern)
+        result = self._non_named_group_matcher.sub('{var}', result)
 
         # handle optional params
-        pattern = self._either_option_matcher.sub(lambda m: m.group(1), pattern)
+        result = self._either_option_matcher.sub(lambda m: m.group(1), result)
 
         # clean up any outstanding regex-y characters.
-        pattern = pattern.replace('^', '').replace('$', '') \
+        result = result.replace('^', '').replace('$', '') \
             .replace('?', '').replace('//', '/').replace('\\', '')
-        if not pattern.startswith('/'):
-            pattern = '/' + pattern
-        return pattern
+
+        return result
+
+    def _resolve(self, resolver, path, parents=None):
+        match = resolver.regex.search(path)
+        if not match:
+            return
+
+        if parents is None:
+            parents = [resolver]
+        else:
+            parents.append(resolver)
+
+        new_path = path[match.end():]
+        for pattern in resolver.url_patterns:
+            # this is an include()
+            if not pattern.callback:
+                match = self._resolve(pattern, new_path, parents)
+                if match:
+                    return match
+                continue
+
+            elif not pattern.regex.search(new_path):
+                continue
+
+            try:
+                return self._cache[pattern]
+            except KeyError:
+                pass
+
+            prefix = ''.join(self._simplify(p.regex.pattern) for p in parents)
+            result = prefix + self._simplify(pattern.regex.pattern)
+            if not result.startswith('/'):
+                result = '/' + result
+            self._cache[pattern] = result
+            return result
 
     def resolve(self, path, urlconf=None):
-        # TODO(dcramer): it'd be nice to pull out parameters
-        # and make this a normalized path
         resolver = get_resolver(urlconf)
-        match = resolver.regex.search(path)
-        if match:
-            new_path = path[match.end():]
-            for pattern in resolver.url_patterns:
-                try:
-                    sub_match = pattern.resolve(new_path)
-                except Resolver404:
-                    continue
-                if sub_match:
-                    pattern = pattern.regex.pattern
-                    try:
-                        return self._cache[pattern]
-                    except KeyError:
-                        pass
-
-                    pattern_name = self._simplify(pattern)
-                    self._cache[pattern] = pattern
-                    return pattern_name
-        return path
+        match = self._resolve(resolver, path)
+        return match or path
