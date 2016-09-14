313bc2be03dfa9365d439e3ac7eee3417417d62f
David Cramer <dcramer@gmail.com>
2016-09-14 09:25:49 -0700

    Bad ref to client

--- raven/middleware.py
+++ raven/middleware.py
@@ -70,7 +70,7 @@ class ClosingIterator(Iterator):
                     self.iterable.close()
         finally:
             self.sentry.client.context.clear()
-            self.sentry.transaction.clear()
+            self.sentry.client.transaction.clear()
             self.closed = True
 
 
