43b98811f837077fd56149856648d3cabd9ee66c
Armin Ronacher <armin.ronacher@active-4.com>
2016-10-18 20:02:16 +0200

    Revert "Merge pull request #882 from do3cc/rest_framework_support"
    This reverts commit db7d970999e7212bc848349ab5bc1e372980ea9d, reversing
    changes made to 7ca8746a8375d69f07de13103fcfbab91a868fc6.
    
    This broke the world

--- raven/contrib/django/client.py
+++ raven/contrib/django/client.py
@@ -34,14 +34,6 @@ from raven._compat import string_types, binary_type
 
 __all__ = ('DjangoClient',)
 
-VALID_HTTP_REQUESTS = (HttpRequest, )
-
-try:
-    from rest_framework.request import Request
-    VALID_HTTP_REQUESTS += (Request, )
-except ImportError:
-    pass
-
 
 class _FormatConverter(object):
 
@@ -265,7 +257,7 @@ class DjangoClient(Client):
         if request is None:
             request = getattr(SentryLogMiddleware.thread, 'request', None)
 
-        is_http_request = isinstance(request, VALID_HTTP_REQUESTS)
+        is_http_request = isinstance(request, HttpRequest)
         if is_http_request:
             data.update(self.get_data_from_request(request))
 
