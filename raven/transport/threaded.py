69590df07d62d31fc5827c699033389be0a3eafa
Chronial <git@chron.visiondesigns.de>
2017-01-14 13:28:58 +0100

    Minor readability improvement

--- raven/transport/threaded.py
+++ raven/transport/threaded.py
@@ -60,9 +60,7 @@ class AsyncWorker(object):
             timeout = self.options['shutdown_timeout']
 
             # wait briefly, initially
-            initial_timeout = 0.1
-            if timeout < initial_timeout:
-                initial_timeout = timeout
+            initial_timeout = min(0.1, timeout)
 
             if not self._timed_queue_join(initial_timeout):
                 # if that didn't work, wait a bit longer
