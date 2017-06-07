e1fa24f0857525c4c7b46450792ae0548d40e84e
Jim Fulton <jim@jimfulton.info>
2017-06-07 15:24:59 -0400

    Added support for logging integreation via ZConfig:
      http://zconfig.readthedocs.io/en/latest/using-logging.html
    
    (Note that the Zope integration also uses ZConfig, but adds lots of
     Zope-specific machinery that won't work elsewhere.)

---
+++ raven/contrib/zconfig/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+raven.contrib.zope
+~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2010-2013 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+import ZConfig.components.logger.factory
+import raven.handlers.logging
+
+class Factory(ZConfig.components.logger.factory.Factory):
+
+    def getLevel(self):
+        return self.section.level
+
+    def create(self):
+        return raven.handlers.logging.SentryHandler(**self.section.__dict__)
+
+    def __init__(self, section):
+        ZConfig.components.logger.factory.Factory.__init__(self)
+        self.section = section
