141c1b621e0bbce94b5c409274ae29754305199c
Alex Tomkins <tomkins@darkzone.net>
2017-07-10 22:30:10 +0100

    Fix initialize import for Django < 1.7
    Fixes #1013 (honest!)

--- raven/contrib/django/__init__.py
+++ raven/contrib/django/__init__.py
@@ -7,13 +7,6 @@ raven.contrib.django
 """
 from __future__ import absolute_import
 
-import django
-
 default_app_config = 'raven.contrib.django.apps.RavenConfig'
 
 from .client import DjangoClient  # NOQA
-
-# Django 1.8 uses ``raven.contrib.apps.RavenConfig``
-if django.VERSION < (1, 7, 0):
-    from .models import initialize
-    initialize()
