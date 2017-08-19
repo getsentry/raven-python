6b356222406b06b39c3a3072bab6920629d8daf3
Eugene Prikazchikov <eprikazc@gmail.com>
2017-08-19 08:26:01 +0300

    Update DjangoRestFrameworkCompatMiddleware to not require get_response in constructor
    This is for compatibility with Django 1.10 and earlier

--- raven/contrib/django/middleware/__init__.py
+++ raven/contrib/django/middleware/__init__.py
@@ -121,7 +121,7 @@ SentryLogMiddleware = SentryMiddleware
 
 
 class DjangoRestFrameworkCompatMiddleware():
-    def __init__(self, get_response):
+    def __init__(self, get_response=None):
         self.get_response = get_response
 
     def __call__(self, request):
