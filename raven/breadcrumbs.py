c4403f21973138cd20cf9c005da4fb934836d76e
Kevin James <kevin@talkiq.com>
2018-09-10 14:40:08 -0700

    chore(deprecation): fix collections.abc imports
    As of Python 3.7, importing ABCs directly from collections has been
    deprecated.
    
    > DeprecationWarning: Using or importing the ABCs from 'collections'
    > instead of from 'collections.abc' is deprecated, and in 3.8 it
    > will stop working.

--- raven/breadcrumbs.py
+++ raven/breadcrumbs.py
@@ -1,9 +1,13 @@
 from __future__ import absolute_import
 
-import collections
 import os
 import logging
 
+try:
+    from collections.abc import Mapping
+except ImportError:
+    # Python < 3.3
+    from collections import Mapping
 from time import time
 from types import FunctionType
 
@@ -140,7 +144,7 @@ def _record_log_breadcrumb(logger, level, msg, *args, **kwargs):
         data_value = kwargs
         data_value.update(extra)
 
-        if args and len(args) == 1 and isinstance(args[0], collections.Mapping) and args[0]:
+        if args and len(args) == 1 and isinstance(args[0], Mapping) and args[0]:
             format_args = args[0]
             data_value.update(format_args)
 
