a07de92427c318931efd642bcda0073b558e3781
Thomas Klausner <tk@giga.or.at>
2022-01-27 11:11:58 +0100

    conftest: get_marker has been renamed to get_closest_marker

--- conftest.py
+++ conftest.py
@@ -74,6 +74,6 @@ def user_instance(request, admin_user):
 
 @pytest.fixture(autouse=True)
 def has_git_requirements(request, project_root):
-    if request.node.get_marker('has_git_requirements'):
+    if request.node.get_closest_marker('has_git_requirements'):
         if not os.path.exists(os.path.join(project_root, '.git', 'refs', 'heads', 'master')):
             pytest.skip('skipped test as project is not a git repo')
